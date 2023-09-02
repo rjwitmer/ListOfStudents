a2baf475cddaf0d8fb944155fc4520317fad762d
Bob Witmer <bwitmer@golden.net>
2023-09-02 19:27:31 -0400

    Initial Commit

---
+++ ListOfStudents/ListOfStudentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfStudentsApp.swift
+//  ListOfStudents
+//
+//  Created by Bob Witmer on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct ListOfStudentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
